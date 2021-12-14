f80bba8bac38ba529e3ee92dd749cd315faa5dc1
Jupyo Hong <jupyo@coconutsilo.com>
2021-12-14 18:49:43 +0900

    fix: update resizing logic

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
-const sharp = require("sharp");
 const aws = require("aws-sdk");
+const sharp = require("sharp");
 const s3 = new aws.S3();
-
-const resizeOptions = [
-  { name: "w140", width: 140 },
-  { name: "w600", width: 600 },
-];
 
 exports.handler = async (event) => {
   try {
-    const Key = event.Records[0].s3.object.key;
-    const keyOnly = Key.split("/")[1];
-    const image = await s3
-      .getObject({ Bucket: "image-upload-test-coconutsilo", Key })
-      .promise();
+    // console.log(
+    //   "Reading options from event:\n",
+    //   util.inspect(event, { depth: 5 })
+    // );
+    const srcBucket = event.Records[0].s3.bucket.name;
+    const srcKey = decodeURIComponent(
+      event.Records[0].s3.object.key.replace(/\+/g, " ")
+    );
+    // 리사이즈 여부 확인
+    const folder = srcKey.split("/")[0];
+    if (!["beverage", "profile"].includes(folder)) {
+      return;
+    }
+
+    const dstBucket = srcBucket + "-resized";
+
+    // S3 원본 버켓에서 이미지 가져오기
+    const params = {
+      Bucket: srcBucket,
+      Key: srcKey,
+    };
+    const srcImage = await s3.getObject(params).promise();
+
+    // 이미지 리사이즈
+    const resizeOptions = [
+      { name: "w250", width: 250 },
+      { name: "w500", width: 500 },
+    ];
+
     await Promise.all(
       resizeOptions.map(async ({ name, width }) => {
-        const newKey = `${name}/${keyOnly}`;
-        const resizedImage = await sharp(image.Body)
-          .rotate() // preserve original images' rotation
-          .resize(width)
-          .toBuffer();
-        await s3
-          .putObject({
-            Bucket: "image-upload-test-coconutsilo",
-            Body: resizedImage,
-            Key: newKey,
-          })
-          .promise();
+        try {
+          const dstKey = `${name}/${srcKey}`;
+          const buffer = await sharp(srcImage.Body)
+            .rotate() // 원본 이미지 회전값 유지
+            .resize({ width, height: width, fit: "outside" })
+            .toBuffer();
+          const dstParams = {
+            Bucket: dstBucket,
+            Key: dstKey,
+            Body: buffer,
+            ContentType: "image",
+          };
+          await s3.putObject(dstParams).promise();
+        } catch (err) {
+          throw err;
+        }
       })
     );
-
-    return {
-      statusCode: 200,
-      body: event,
-    };
-  } catch (err) {}
+    console.log("Successfully resized " + srcBucket + "/" + srcKey);
+    return;
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 };
